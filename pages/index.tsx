234a3c992875beb67b08fe5731c11c95a58e2dbe
BossTI <igejuwon53@gmail.com>
2022-01-26 02:05:32 -0800

    index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import { PostCard, Categories, PostWidget } from '../components'
-import { getPosts } from '../services'
+import { getPosts, getRecentPosts, getSimilarPosts } from '../services'
 import { FeaturedPosts } from '../sections';
 
 export default function Home({ posts }) {
@@ -19,7 +19,7 @@ export default function Home({ posts }) {
         </div>        
         <div className="lg:col-span-4 col-span-1">
             <div className="lg:sticky relative top-8">
-              <PostWidget categories={posts} slug={posts} />
+              <PostWidget categories={getRecentPosts} slug={getSimilarPosts} />
               <Categories />
             </div>
         </div>
