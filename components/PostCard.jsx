f8c9c5037e8aba95ce90fb7c9734b20a78b80b8a
BossTI <igejuwon53@gmail.com>
2022-01-25 16:33:15 -0800

    updated a file

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -7,7 +7,7 @@ console.log(post)
 
   return (
     <div className="bg-gray-200 shadow-lg rounded-lg p-0 lg:p-8 pb-12 mb-8">
-      <div className="relative shadow-md inline-block w-full h-96 lg:h-96 mb-6">
+      <div className="relative shadow-md inline-block w-full h-96 lg:h-80 mb-6">
         <img
           src={post.featuredImage.url}
           alt={post.title}
