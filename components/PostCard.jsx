e5b5b8c9af5d05b9ffdd9c705d98fdae3b22bcb6
BossTI <igejuwon53@gmail.com>
2022-01-25 16:30:43 -0800

    updated 2 files

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -11,7 +11,7 @@ console.log(post)
         <img
           src={post.featuredImage.url}
           alt={post.title}
-          className="object-top absolute h-96 w-full object-cover shadow-lg rounded-t-lg lg:rounded-lg"
+          className="object-top absolute h-80 w-full object-cover shadow-lg rounded-t-lg lg:rounded-lg"
         />
       </div>
       <h1 className="transition duration-700 text-center mb-8 cursor-pointer hover:text-pink-600 text-3xl font-semibold">
--- pages/index.tsx
+++ pages/index.tsx
@@ -19,7 +19,7 @@ export default function Home({ posts }) {
         </div>        
         <div className="lg:col-span-4 col-span-1">
             <div className="lg:sticky relative top-8">
-              <PostWidget categories={undefined} slug={undefined} />
+              <PostWidget categories={posts} slug={posts} />
               <Categories />
             </div>
         </div>
